7026f8b2bb184b234aae4cde7e8f6a4c88213e92
Paul Pietzko <paul.pietzko@icloud.com>
2026-01-01 17:41:28 +0100

    feat: update social links

--- src/lib/sociallinks.ts
+++ src/lib/sociallinks.ts
@@ -1,6 +1,6 @@
 export const links = [
-  { href: "https://www.instagram.com/aspecsgaming/", label: "Instagram" },
+  { href: "https://www.instagram.com/aspecsweb/", label: "Instagram" },
   { href: "https://aspecsgaming.itch.io", label: "Itch.io" },
-  { href: "https://www.youtube.com/@aspecsgaming", label: "YouTube" },
+  { href: "https://www.youtube.com/@aspecsweb", label: "YouTube" },
   { href: "https://www.gaming.aspecs.ch", label: "Website" },
 ];
